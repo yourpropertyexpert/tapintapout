31da82b1284db5d8509c3012ef42f26041761cd2
Mark Harrison <yourpropertyexpert@gmail.com>
2021-03-12 14:16:02 +0000

    403 vs 401 return codes, PSR-12

--- api/src/endpoints/setlocation.php
+++ api/src/endpoints/setlocation.php
@@ -3,14 +3,9 @@
 namespace yourpropertyexpert;
 
 require_once("../includes/autoloaders.php");
-require_once("../includes/checkkey.php"); // sets up array called $checkresult if check passes, exits page otherwise
+$user = require_once("../includes/checkkey.php");
 
 $return = 200; // all good
 
-$data = [];
-$data = ["user" => $checkresult["user"]];
-
-new Output($return, $data);
-
-
+new Output($return, $user);
 exit();
--- api/src/includes/checkkey.php
+++ api/src/includes/checkkey.php
@@ -1,26 +1,26 @@
 <?php
-
 /**
  * include file to check for key
  */
 
 namespace yourpropertyexpert;
 
+if (!array_key_exists("userkey", $_REQUEST)) {
+    $return = 401; // unauthorised, which in practice means unauthenticated
+    $data = ["authenticated" => false, "error" => "userkey not provided"];
+    new Output($return, $data);
+    exit();
+}
 
- if (!array_key_exists("userkey", $_REQUEST)) {
-     $return = 401; // unauthorised, which in practice means unauthenticated
-     $data = ["authenticated" => false, "error" => "userkey not provided"];
-     new Output($return, $data);
-     exit();
- }
+$keycheck = new KeyCheck($_REQUEST["userkey"]);
+$checkresult = $keycheck->checkKey();
 
- $keycheck = new KeyCheck($_REQUEST["userkey"]);
- $checkresult = $keycheck->checkKey();
+if (!$checkresult["authenticated"]) {
+    $return = 403; // forbidden, which is to say the credentials don't match
+    $checkresult["userkey"] = $_REQUEST["userkey"];
+    $checkresult["error"] = "userkey does not match any users";
+    new Output($return, $checkresult);
+    exit();
+}
 
- if (!$checkresult["authenticated"]) {
-     $return = 401; // unauthorised, which in practice means unauthenticated
-     $checkresult["userkey"] = $_REQUEST["userkey"];
-     $checkresult["error"] = "userkey does not match any users";
-     new Output($return, $checkresult);
-     exit();
- }
+return $checkresult;
